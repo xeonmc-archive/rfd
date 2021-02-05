12e88e29b81cf777e32554e35b0a3ae4d8aebb04
xeonmc <77922942+xeonmc@users.noreply.github.com>
2021-02-05 09:44:59 +0800

    Update discordbot.js

--- dc/discordbot.js
+++ dc/discordbot.js
@@ -28,19 +28,21 @@ console.log(creds.channelID);
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     chokidar.watch(pathToReplays).on('add', (filepath, event) => {
-        console.log("new file " + filepath);
+        console.log("New file? " + filepath);
         if ( filepath.endsWith(".rep") && !(uploadHistory.list.includes(filepath)) ) {
             const filename = path.basename(filepath, path.extname(filepath));
             const destfile = pathToReplays+filename+".zip";
-            console.log("haven't seen " + filename + " before. Uploading...");
+            console.log("Haven't seen " + filename + " before. Uploading...");
             compressing.zip.compressFile(filepath, destfile)
-            .then((value) => {
-                console.log(value);
+            .then(() => {
                 const attachment = new Discord.MessageAttachment(destfile);
                 client.channels.cache.get(creds.channelID).send(attachment)
-                    .then(console.log).catch(console.error);
-                uploadHistory.list.push(filepath);
-                fs.writeFileSync(pathToSentLog, JSON.stringify(uploadHistory, null, 2));
+                .then((value) => {
+                    console.log(value);
+                    console.log("Upload finished. Writing " + filepath + " to upload history.");
+                    uploadHistory.list.push(filepath);
+                    fs.writeFileSync(pathToSentLog, JSON.stringify(uploadHistory, null, 2));
+                }).catch(console.error);
             }).catch(console.error);
         }
     });
